f19344a179d83df2e6635ccb6e8f040468f27e0d
tomtechstarter <149696023+tomtechstarter@users.noreply.github.com>
2024-03-07 11:01:20 +0100

    Add Test For  Get By Id Route

--- unit-testing/tag-3/asynchrone-tests-mocks/TodosQueries.js
+++ unit-testing/tag-3/asynchrone-tests-mocks/TodosQueries.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 async function fetchAllTodos() {
-  const result = await axios.get("http://localhost:5051/v1/todos/all");
+  const result = await axios.get("http://localhost:5050/v1/todos/all");
 
   const todos = result.data;
 
@@ -9,7 +9,7 @@ async function fetchAllTodos() {
 }
 
 async function fetchTodoById(todoId) {
-  const result = await api.get("http://localhost:5051/v1/todos/byid", {
+  const result = await axios.get("http://localhost:5050/v1/todos/byid", {
     params: { todoId },
   });
 
